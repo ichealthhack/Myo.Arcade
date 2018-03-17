fb4e5e4280abd3991be3df7f7ac3934dae758ba1
passwordIsBurrito <georgewrites@gmail.com>
2018-03-17 13:11:52 +0000

    Added Roll-a-ball

---
+++ Roll-a-Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate is guarenteed to run after all items have been procesed in Update
+	void LateUpdate ()
+    {
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ Roll-a-Ball/Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    //We are not using forces, so we can use "Update" rather than "FixedUpdate"
+    void Update ()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+	}
+}
